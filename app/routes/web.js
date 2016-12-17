d180e3f92ee75b8087b9870d812decc35ef9479c
Stian Westvig <stian.westvig@gmail.com>
2016-12-17 21:42:43 +0100

    added two more routes and a menu

--- app/routes/web.js
+++ app/routes/web.js
@@ -11,4 +11,12 @@ router.get('/', (req, res) => {
   res.render('home', context);
 });
 
+router.get('/del', (req, res) => {
+  res.render('form', context);
+});
+
+router.get('/tilbud', (req, res) => {
+  res.render('offer', context);
+});
+
 module.exports = router;
